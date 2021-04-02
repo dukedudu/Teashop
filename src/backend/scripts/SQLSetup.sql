0de456138fe536695cfbdece192f3018d88e27f3
eric369java <55117363+eric369java@users.noreply.github.com>
2021-04-01 19:16:50 -0700

    Update SQLSetup.sql

--- src/backend/scripts/SQLSetup.sql
+++ src/backend/scripts/SQLSetup.sql
@@ -1,121 +1,193 @@
-CREATE TABLE Address(
-PostalCode CHAR(20),
-StreetName CHAR(20),
-House# INT,
-City CHAR(20),
-PRIMARY KEY(PostalCode, StreetName, House#));
-
-CREATE TABLE User(
-UName CHAR(20) PRIMARY KEY,
-Password CHAR(20) NOT NULL,
-PostalCode CHAR(20) NOT NULL,
-StreetName CHAR(20) NOT NULL,
-House# INT NOT NULL,
-Certificate CHAR(20),
-Budget INT,
-FOREIGN KEY(PostalCode) REFERENCES Address(PostalCode) ON DELETE CASCADE,
-FOREIGN KEY(StreetName) REFERENCES Address(StreetName) ON DELETE CASCADE,
-FOREIGN KEY(House#) REFERENCES Address(House#) ON DELETE CASCADE));
-
-CREATE TABLE Grocery(
-GName CHAR(20) PRIMARY KEY,
-Amount INT DEFAULT 0,
-BuyingDate DATE,
-Duration INT,
-FOREIGN KEY BuyingDate REFERENCES GroceryDate(BuyingDate) ON DELETE CASCADE,
-FOREIGN KEY Duration REFERENCES GroceryDate(Duration) ON DELETE CASCADE);
-
-CREATE TABLE GroceryDate(
-BuyingDate DATE,
-Duration INT,
-ExpiryDate DATE,
-PRIMARY KEY(BuyingDate, Duration));
-
-CREATE TABLE Buys(
-UserId INT,
-GName CHAR(20),
-BuyingDate DATE,
-PRIMARY(UserId, GName, BuyingDate),
-FOREIGN KEY(UserId) REFERENCES User(UserId) ON DELETE CASCADE,
-FOREIGN KEY(GName) REFERENCES Grocery(GName) ON DELETE CASCADE,
-FOREIGN KEY(BuyingDate) REFERENCES Grocery(BuyingDate) ON DELETE CASCADE);
+--Create all tables.
+CREATE TABLE Users(
+    UName VARCHAR2(20) PRIMARY KEY,
+    Password VARCHAR2(20) NOT NULL,
+    StreetName VARCHAR2(20) NOT NULL,
+    HouseNumber INT DEFAULT 0,
+    City VARCHAR2(20) NOT NULL,
+    PostalCode VARCHAR2(20) NOT NULL);
 
 CREATE TABLE Recipe(
-RecipeId INT PRIMARY KEY,
-RName CHAR(20),
-Tea CHAR(20) NOT NULL,
-Calories INT DEFAULT 0,
-Perl INT DEFAULT 0,
-Jelly INT DEFAULT 0,
-Lemon INT DEFAULT 0,
-Orange INT DEFAULT 0);
+    RName VARCHAR2(20) PRIMARY KEY,
+    Kind VARCHAR2(20) NOT NULL,
+    Pearl INT DEFAULT 0,
+    Jelly INT DEFAULT 0,
+    Lemon INT DEFAULT 0,
+    Orange INT DEFAULT 0);
+
+CREATE TABLE Address(
+    PostalCode VARCHAR2(20),
+    StreetName VARCHAR2(20),
+    House# INT,
+    City VARCHAR2(20),
+    PRIMARY KEY(PostalCode, StreetName, House#));
+
+CREATE TABLE Grocery(
+    GName VARCHAR2(20),
+    Amount INT DEFAULT 0,
+    BuyingDate DATE,
+    Duration INT,
+    ExpiryDate DATE,
+    PRIMARY KEY(GName, BuyingDate));
+
+CREATE TABLE GroceryDate(
+        BuyingDate DATE,
+        Duration INT,
+        ExpiryDate DATE,
+        PRIMARY KEY(BuyingDate, Duration));
+
+CREATE TABLE Buys(
+    UName VARCHAR2(20),
+    GName VARCHAR2(20),
+    BuyingDate DATE,
+    PRIMARY KEY(UName, GName, BuyingDate),
+    FOREIGN KEY(UName) REFERENCES Users,
+    FOREIGN KEY(GName, BuyingDate) REFERENCES Grocery(GName, BuyingDate));
+
+CREATE TABLE MakeRecipe(
+    UName VARCHAR2(20),
+    RName VARCHAR2(20),
+    PRIMARY KEY(UName,RName),
+    FOREIGN KEY(UName) REFERENCES Users,
+    FOREIGN KEY(RName) REFERENCES Recipe);
 
 CREATE TABLE Usage(
-UseID INT PRIMARY KEY,
-RecipeId INT,
-Date DATE NOT NULL,
-Tea CHAR(20) NOT NULL,
-Calories INT DEFAULT 0,
-Perl INT DEFAULT 0,
-Jelly INT DEFAULT 0,
-Lemon INT DEFAULT 0,
-Orange INT DEFAULT 0,
-FOREIGN KEY RecipeId REFERENCES Recipe(RecipeId) ON DELETE SET NULL);
+    UseID INT PRIMARY KEY,
+     VARCHAR2(20),
+     UsingDate DATE NOT NULL,
+     Pearl INT DEFAULT 0,
+     Jelly INT DEFAULT 0,
+     Lemon INT DEFAULT 0,
+     Orange INT DEFAULT 0);
 
 CREATE TABLE Generates(
-RecipeId INT,
-UseID INT,
-PRIMARY KEY(RecipeId, UseID),
-FOREIGN KEY RecipeId REFERENCES Recipe ON DELETE CASCADE,
-FOREIGN KEY UseId REFERENCES Usage ON DELETE CASCADE);
+    RName VARCHAR2(20),
+    UseID INT,
+    PRIMARY KEY(RName, UseID),
+    FOREIGN KEY(RName) REFERENCES Recipe ON DELETE CASCADE,
+    FOREIGN KEY(UseId) REFERENCES Usage(UseID) ON DELETE CASCADE);
 
-CREATE TABLE DailyReport (
-ReportID INT,
-UseID INT,
-Date DATE NOT NULL,
-UserID INT,
-Perl INT DEFAULT 0,
-Jelly INT DEFAULT 0,
-Lemon INT DEFAULT 0,
-Orange INT DEFAULT 0,
-PRIMARY KEY(UseID, ReportID),
-FOREIGN KEY (UserID) REFERENCES User ON DELETE CASCADE,
-FOREIGN KEY (Date) REFERENCES ReportWeekDay ON DELETE CASCADE,
-FOREIGN KEY (Weekday) REFERENCES ReportWeekDay ON DELETE CASCADE);
+CREATE TABLE DailyReport(
+    ReportDay DATE PRIMARY KEY,
+    Pearl INT DEFAULT 0,
+    Jelly INT DEFAULT 0,
+    Lemon INT DEFAULT 0,
+    Orange INT DEFAULT 0);
 
-CREATE TABLE ReportWeekDay (
-Date DATE PRIMARY KEY,
-Weekday INT);
+CREATE TABLE Supplier(
+    SupplierId INT PRIMARY KEY,
+    CompanyName VARCHAR2(20) NOT NULL);
 
-CREATE TABLE Supplier (
-SupplierId INT PRIMARY KEY,
-CompanyName CHAR(20) NOT NULL UNIQUE);
+CREATE TABLE Supplies(
+    SupplierId INT PRIMARY KEY ,
+    GName VARCHAR2(20),
+    BuyingDate DATE);
 
-CREATE TABLE Supplies (
-SupplierId INT,
-GName CHAR(20),
-PRIMARY KEY (SupplierId, GName),
-FOREIGN KEY (SupplierId) REFERENCES Supplier ON DELETE CASCADE,
-FOREIGN KEY (GName) REFERENCES Grocery ON DELETE CASCADE);
+CREATE TABLE ShoppingList(
+    GName VARCHAR2(20),
+    Amount INT NOT NULL,
+    ListDate DATE,
+    PRIMARY KEY (GName,ListDate));
 
-CREATE TABLE ShoppingList (
-SListId INT PRIMARY KEY,
-GName CHAR(20),
-Amount INT NOT NULL,
-Date DATE,
-UserId INT,
-FOREIGN KEY (UserId) REFERENCES User ON DELETE CASCADE,
-FOREIGN KEY (GName) REFERENCES Grocery ON DELETE CASCADE);
+CREATE TABLE Lists(
+    GName VARCHAR2(20),
+    ListDate DATE,
+    UseID INT,
+    PRIMARY KEY (UseID, GName, ListDate));
 
-CREATE TABLE Lists (
-UseID INT,
-GName CHAR(20),
-SListId INT,
-PRIMARY KEY (SListId, UseID, GName),
-FOREIGN KEY (SListId) REFERENCES ShoppingList ON DELETE CASCADE,
-FOREIGN KEY (GName) REFERENCES Grocery ON DELETE CASCADE,
-FOREIGN KEY (UseId) REFERENCES Usage ON DELETE CASCADE);
+--Insert all Users.
 
+INSERT INTO Users VALUES ('Sam', '123', '23rd W Ave', 2341, 'Vancouver', 'V6S1H6');
+INSERT INTO Users VALUES ('Lily', '234', '23rd W Ave', 2341, 'Richmond', 'V6S1H6');
+INSERT INTO Users VALUES ('Amy', '456', '78th S Ave', 4562, 'Richmond', 'V6S1H6');
+INSERT INTO Users VALUES ('Frank', '567', 'King St', 6753, 'Burnaby', 'V6S1H6');
+INSERT INTO Users VALUES ('Jasmine', '678', 'Queen St', 2413, 'Vancouver', 'H3M2N9');
 
--- INSERT INTO branch VALUES (1, "ABC", "123 Charming Ave", "Vancouver", "6041234567");
--- INSERT INTO branch VALUES (2, "DEF", "123 Coco Ave", "Vancouver", "6044567890");
+--Insert all Recipes
+
+INSERT INTO Recipe VALUES ('Pearl Milk Tea', 'Black Tea', 20, 0, 0, 0);
+INSERT INTO Recipe VALUES ('Jelly Milk Tea', 'Black Tea', 0, 20, 0, 0);
+INSERT INTO Recipe VALUES ('Pearl Jelly Milk Tea', 'Green Tea', 10, 10, 0, 0);
+INSERT INTO Recipe VALUES ('Lemon Tea', 'Green Tea', 0, 0, 2, 0);
+INSERT INTO Recipe VALUES ('Orange Tea', 'Green Tea', 0, 0, 0, 2);
+
+--Insert all Address
+INSERT INTO Address VALUES ('V6S1H6', '23rd W Ave', 2341, 'Vancouver');
+INSERT INTO Address VALUES ('V3T1K4', 'Binary Ave', 482, 'Richmond');
+INSERT INTO Address VALUES ('V4Z2T3', '78th S Ave',	4562, 'Richmond');
+INSERT INTO Address VALUES ('H3K2B8', 'King St', 6753, 'Burnaby');
+INSERT INTO Address VALUES ('H3M2N9', 'Queen St', 2413,	'Vancouver');
+
+--Insert all Grocery
+INSERT INTO Grocery VALUES ('Pearl', 300, '2020-02-14', 10, '2020-02-24');
+INSERT INTO Grocery VALUES ('Jelly', 200, '2021-02-16', 10, '2021-02-24');
+INSERT INTO Grocery VALUES ('Orange', 10, '2021-02-05', 20, '2021-02-25');
+INSERT INTO Grocery VALUES ('Lemon', 50, '2021-02-01', 23, '2021-02-24');
+
+--Insert all Grocery Date
+INSERT INTO GroceryDate VALUES ('2020-02-14', 10, '2020-02-24');
+INSERT INTO GroceryDate VALUES ('2021-02-16', 10, '2021-02-24');
+INSERT INTO GroceryDate VALUES ('2021-02-05', 20, '2021-02-25');
+INSERT INTO GroceryDate VALUES ('2021-02-01', 23, '2021-02-24');
+
+--Insert all Buys
+INSERT INTO Buys VALUES ('Sam', 'Pearl', '2021-02-14');
+INSERT INTO Buys VALUES ('Sam', 'Jelly', '2021-02-16');
+INSERT INTO Buys VALUES ('Amy', 'Orange', '2021-02-05');
+INSERT INTO Buys VALUES ('Frank', 'Lemon', '2021-02-01');
+
+--Insert all MakeRecipe
+INSERT INTO MakeRecipe VALUES ('Sam', 'Pearl Milk Tea');
+INSERT INTO MakeRecipe VALUES ('Lily', 'Pearl Milk Tea');
+INSERT INTO MakeRecipe VALUES ('Amy', 'Lemon Tea');
+INSERT INTO MakeRecipe VALUES ('Frank', 'Orange Tea');
+INSERT INTO MakeRecipe VALUES ('Jasmine', 'Pearl Jelly Milk Tea');
+
+--Insert all Usage
+INSERT INTO Usage VALUES (1, 'Pearl Milk Tea', '2021-01-01', 20, 0, 0, 0);
+INSERT INTO Usage VALUES (2, 'Pearl Milk Tea', '2021-01-01', 20, 0, 0, 0);
+INSERT INTO Usage VALUES (3, 'Jelly Milk Tea', '2021-01-03', 0, 20, 0, 0);
+INSERT INTO Usage VALUES (4, 'Pearl Jelly Milk Tea', '2021-01-04', 10, 10, 0, 0);
+INSERT INTO Usage VALUES (5, 'Orange Tea', '2021-01-05', 0, 0, 2, 0);
+
+--Insert All Generates
+INSERT INTO Generates VALUES ('Pearl Milk Tea', 1);
+INSERT INTO Generates VALUES ('Pearl Milk, Tea', 2);
+INSERT INTO Generates VALUES ('Jelly Milk Tea', 3);
+INSERT INTO Generates VALUES ('Pearl Jelly Milk Tea', 4);
+INSERT INTO Generates VALUES ('Orange Tea', 5);
+
+--Insert All DailyReport
+INSERT INTO DailyReport VALUES ('2021-01-01', 20, 0, 0, 0);
+INSERT INTO DailyReport VALUES ('2021-01-02', 0, 0, 0, 0);
+INSERT INTO DailyReport VALUES ('2021-01-03', 40, 20, 0, 0);
+INSERT INTO DailyReport VALUES ('2021-01-04', 40, 40, 20, 20);
+INSERT INTO DailyReport VALUES ('2021-01-05', 0, 0, 0, 0);
+
+--Insert All Supplier
+INSERT INTO Supplier VALUES (1, 'Costco');
+INSERT INTO Supplier VALUES (2, 'T&T Supermarket');
+INSERT INTO Supplier VALUES (3, 'No Frills');
+INSERT INTO Supplier VALUES (4, 'Safeway');
+INSERT INTO Supplier VALUES (5, 'SaveOnFoods');
+
+--Insert All Supplies
+INSERT INTO Supplies VALUES (1, 'Pearl', '2021-01-01');
+INSERT INTO Supplies VALUES (2, 'Jelly', '2021-01-02');
+INSERT INTO Supplies VALUES (3, 'Orange', '2021-01-03');
+INSERT INTO Supplies VALUES (4, 'Lemon', '2021-01-04');
+INSERT INTO Supplies VALUES (5, 'Pearl', '2021-01-03');
+
+--Insert All ShoppingLists
+INSERT INTO ShoppingList VALUES ('Pearl', 300, '2021-01-01');
+INSERT INTO ShoppingList VALUES ('Jelly', 200, '2021-01-01');
+INSERT INTO ShoppingList VALUES ('Orange', 300, '2021-01-03');
+INSERT INTO ShoppingList VALUES ('Lemon', 10, '2021-01-04');
+INSERT INTO ShoppingList VALUES ('Pearl', 300, '2021-01-04');
+
+--Insert All Lists
+INSERT INTO Lists VALUES ('Pearl', '2021-01-01', 1);
+INSERT INTO Lists VALUES ('Jelly', '2021-01-03', 3);
+INSERT INTO Lists VALUES ('Orange', '2021-01-05', 5);
+INSERT INTO Lists VALUES ('Pearl', '2021-01-01', 2);
+INSERT INTO Lists VALUES ('Pearl', '2021-01-04', 4); 
